11c7ea94447647d1d1d6ba972406ec76df4132b1
Christian Epple <christian.epple@aisec.fraunhofer.de>
2021-03-11 14:28:06 +0100

    [WIP] Reduce CML interface to be minimal
    In order to prepare Trust|me to be evaluated according to CC
    we need to reduce all interfaces to the absolute minimum.
    
    This commits reduces the interfaces that are implmented via the
    control and service sockets. To avoid having to maintain a hard
    fork, a new preprocesser flag, "CC_Mode" is being introduced.
    This flag will effectively filter commands that the cmld services.
    
    In order for clients to be able to implement the reduced interfaces
    we supply the specification in form of adapted .proto files.
    
    TODOs:
    - implement control command filtering in daemon/control.c
    - reflect reduced command set in daemon/control.proto
    - implement reduced service interface in cmld
    - properly comment all .proto files so that developer documentation
      can be built from them automatically
    
    Signed-off-by: Christian Epple <christian.epple@aisec.fraunhofer.de>
    
    Added enforcement of CC control.proto, device_config.proto and guestos.proto
    
    Work on CC_Mode
    
    Work
    
    CC Work
    
    CC Work
    
    CC Work
    
    CC Work
    
    CC Work
    
    CC work

--- daemon/device_config.c
+++ daemon/device_config.c
@@ -64,6 +64,22 @@ device_config_new(const char *path)
 		}
 	}
 	ASSERT(cfg);
+	// check cfg here as its ensured to be loaded
+	//#ifdef CC_MODE
+	//	/* TODO:
+	//	 * - enforce CC compatible configuration
+	//	 */
+	//
+	//	if (	(cfg->mdm_node) || (cfg->mdm_service) ||
+	//			(cfg->telephony_uuid) || (cfg->should_led_blink) ||
+	//			(cfg->c0os) || (cfg->locally_signed_images) ||
+	//			(cfg->hostedmode) || (cfg->signed_configs))
+	//	{
+	//		WARN("Device config uses options that are prohibited in CC Mode!");
+	//		return NULL;
+	//	}
+	//
+	//#endif
 
 	device_config_t *config = mem_new0(device_config_t, 1);
 	config->file = file;
